1d9eb2a24b61a94ad1dc67c32946878fd07f26a7
graycadeau <g.zawadi.w@gmail.com>
2018-08-28 09:09:18 +0300

    [[ft #160029401]test user

--- tests/unit/api_v1/test_user.py
+++ tests/unit/api_v1/test_user.py
@@ -19,11 +19,12 @@ class TestUser(unittest.TestCase):
             data = json.dumps(user_info),
             content_type = 'application/json'
         )
+        
         return res
 
-    def test_user_signup(self):
+    def test_signup_user(self):
         res = self.signup_user()
-        self.assertEqual(res.status_code, 200)
+        self.assertEqual(res.status_code, 201)
 
     def login_user(self):
         login_info = {
@@ -37,9 +38,9 @@ class TestUser(unittest.TestCase):
         )
         return res
 
-    def test_login(self):
+    def test_login_user(self):
         res = self.login_user()
-        self.assertEqual(res.status_code, 200)
+        self.assertEqual(res.status_code, 201)
 
 if __name__ == "__main__":
     unittest.main()
