6970403196f15eddf725bd2bcacac81aab17bd93
graycadeau <g.zawadi.w@gmail.com>
2018-08-28 09:08:01 +0300

    [[ft #160029401]test answers

--- tests/unit/api_v1/test_answers.py
+++ tests/unit/api_v1/test_answers.py
@@ -3,29 +3,27 @@ import json
 
 from app import app
 
-class TestUser(unittest.TestCase):
+class TestAnswer(unittest.TestCase):
     def setUp(self):
         app.testing = True
         self.app = app.test_client()
 
-    def post_question(self):
-        query_info = {
-            "How do I refactor tests with database?"
+    def post_answer(self):
+        question = {
+            "question":"How do I refactor tests with database?"
         }
 
-        res = self.app.post('/app/v1/questions',
-            data = json.dumps(query_info),
+        res = self.app.post('/api/v1/questions/',
+            data = json.dumps(question),
             content_type = 'application/json'
         )
-        return res
 
-    def post_answer(self):
-        res_info = {
+        answer = {
             "response":"You could first of all seperate your tests?"
         }
 
-        res = self.app.post('/app/v1/questions/4/ans',
-            data = json.dumps(res_info),
+        res = self.app.post('/api/v1/questions/2/answers',
+            data = json.dumps(answer),
             content_type = 'application/json'
         )
         return res
@@ -34,20 +32,9 @@ class TestUser(unittest.TestCase):
         res = self.post_answer()
         self.assertEqual(res.status_code,201)
 
-    def update_answer(self):
-        update_info = {
-            "response":"You could first of all seperate your tests e.g test_base,test_auth etc"
-        }
-
-        res = self.app.put('/api/v1/questions/4/ans/6',
-            data = json.dumps(update_info),
-            content_type = 'application/json'
-        )
-        return res
-
-    def test_update_answer(self):
-        res = self.update_answer()
-        self.assertEqual(res.status_code,201)
+    def test_get_answers(self):
+        res = self.app.get('/api/v1/questions/1/answers')
+        self.assertEqual(res.status_code, 200)
 
 if __name__ == "__main__":
     unittest.main()
