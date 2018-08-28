619f2038c7c0b03f51cb06f64cb8e45ca0935ac2
graycadeau <g.zawadi.w@gmail.com>
2018-08-28 09:08:56 +0300

    [[ft #160029401]test questions

--- tests/unit/api_v1/test_questions.py
+++ tests/unit/api_v1/test_questions.py
@@ -3,39 +3,73 @@ import json
 
 from app import app
 
-class TestUser(unittest.TestCase):
+
+class TestQuestion(unittest.TestCase):
     def setUp(self):
         app.testing = True
         self.app = app.test_client()
 
     def post_question(self):
         query_info = {
-            "question":"How do I refactor tests with database?"
+            "question": "How do I refactor tests with database?"
         }
 
-        res = self.app.post('/api/v1/questions',
-            data = json.dumps(query_info),
-            content_type = 'application/json'
-        )
-    
-        return res
-        
+        res = self.app.post('/api/v1/questions/',
+                            data=json.dumps(query_info),
+                            content_type='application/json'
+                            )
 
-    def test_post_questions(self):
+        return res
+
+    def test_post_question(self):
         res = self.post_question()
         self.assertEqual(res.status_code, 201)
 
     def test_get_all_questions(self):
-        res = self.app.get('/api/v1/questions')
+        res = self.app.get('/api/v1/questions/')
         self.assertEqual(res.status_code, 200)
 
     def test_get_specific_question(self):
-        res = self.app.get('/api/v1/questions/6')
+        """post question"""
+        question = {
+            "question": "How do I refactor tests with database?"
+        }
+
+        res = self.app.post('/api/v1/questions/',
+                            data=json.dumps(question),
+                            content_type='application/json'
+                            )
+
+        res = self.app.get('/api/v1/questions/2')
         self.assertEqual(res.status_code, 200)
+
+    def test_update_question(self):
+        question = {
+            "question": "How do I refactor tests to pass?"
+        }
+
+        res = self.app.put('/api/v1/questions/2',
+                           data=json.dumps(question),
+                           content_type='application/json')
+
+        self.assertEqual(res.status_code, 201)
+    
 
     def test_delete_specific_question(self):
-        res = self.app.get('/api/v1/questions/8')
+        """post question"""
+        question = {
+            "question": "How do I refactor tests with database?"
+        }
+
+        res = self.app.post('/api/v1/questions/',
+                            data=json.dumps(question),
+                            content_type='application/json'
+                            )
+
+        """delete question"""
+        res = self.app.delete('/api/v1/questions/3')
         self.assertEqual(res.status_code, 200)
 
+    
 if __name__ == "__main__":
     unittest.main()
